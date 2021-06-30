cbcf4fcfc1064c8c3ba61b06b3c11062a46e4343
raheeljadoon <raheeljadoon56@gmail.com>
2021-06-30 01:34:41 -0400

    update

---
+++ Documents/Assignmet/assign/names.py
@@ -0,0 +1,47 @@
+Mother_Name = ["Jessica","Ince","Zoe","Poole","Melanie","Randall","Madeleine","Manning","Jennifer","Berry","Jessica","Ellison","Sophie","Rees",
+"Faith","Sutherland","Jennifer","Black","Deirdre","Peters","Andrea","Mackay","Caroline","Hamilton","Melanie","MacLeod","Dorothy","Welch","Irene","Lawrence",
+"Sonia","Butler","Bernadette","Stewart"
+"Amy","Hemmings","Alexandra","Reid","Michelle","Hardacre","Ella","Mackenzie","Alexandra","Dyer","Madeleine","Ogden","Dorothy","Ferguson","Emily","Dickens","Zoe",
+"Lawrence","Alison","Arnold","Tracey","Wallace","Natalie","Mitchell","Sarah","Wilson","Maria","Vaughan","Amy",
+"Rees","Stephanie","Randall","Ruth","Clark","Theresa","Anderson","Victoria",
+"Sanderson","Joan","Miller","Anne","Kerr","Sophie","Carr",
+"Una","Knox","Amy","Kerr","Jasmine","Randall","Mary","Rampling","Angela","Short","Jessica","Manning",
+"Jennifer","Ball","Virginia","Jones","Rachel","Harris","Grace","Knox","Deirdre","Short","Heather","Penelope","Jessica","Mary","Diane","Vanessa","Sophie","Diana","Donna","Stephanie","Abigail",	
+"Stephanie"	"Amelia"	"Molly"	"Bernadette"	"Penelope"	"Joan"	"Anna"	"Joanne,Lauren,Anna,Wanda,Amanda,Kimberly","Katherin"	
+"Sue","Ava","Rose","Wanda","Megan","Wendy","Tracey","Michelle","Rose","Alison","Irene","Kimberly","Wendy","Melanie","Emily","Joan","Maria","Katherine","Kylie""Abigail""Gabrielle""Anne","Kimberly",
+"Natalie","Joanne","Deirdre","Lisa"	"Rachel"	"Karen"	"Karen,Leah"	"Heather"	
+"Melanie","Wanda","Chloe","Jessica","Gabrielle","Gabrielle","Molly","Sophie","Pippa","Sonia","Chloe","Amanda","Audrey","Gabrielle","Grace","Lillian","Katherine","Claire,Rachel",	
+"Mary"	"Chloe"	"Julia"	"Hannah,Heather"	
+"Andrea","Michelle","Melanie","Emily","Abigail","Olivia","Emma","Olivia","Angela","Donna","Ruth","Grace","Faith","Megan","Madeleine","Alexandra","Karen","Nicola","Vanessa","Lisa","Claire",	
+"Michelle","Stephanie","Ruth","Faith","Penelope","Claire","Jane"	
+"Lisa","Kimberly","Sally","Anne","Audrey","Molly","Andrea","Lauren","Rachel","Diane","Julia","Dorothy","Pippa","Claire","Caroline","Vanessa"]
+
+
+
+Father_Name = ["Adam","Adrian","Alan","Alexander","Andrew","Anthony","Austin","Benjamin",
+"Blake","Boris","Brandon","Brian","Cameron","Carl","Charles","Christian","Christopher","Colin","Connor","Dan","David",
+"Dominic","Dylan","Edward","Eric","Evan","Frank","Gavin","Gordon","Harry","Ian","Isaac","Jack","Jacob","Jake","James","Jason","Joe","John","Jonathan","Joseph","Joshua","Julian",
+"Justin","Keith","Kevin","Leonard","Liam","Lucas","Luke","Matt","Max","Michael","Nathan","Neil","Nicholas","Oliver","Owen","Paul","Peter","Phil","Piers","Richard","Robert","Ryan",
+"Sam","Sean","Sebastian","Simon",
+"Stephen","Steven","Stewart","Thomas","Tim","Trevor","Victor","Warren","William"]
+
+Name = ["Abigail","Alexandra","Alison","Amanda","Amelia","Amy","Andrea","Angela","Anna",
+"Anne","Audrey","Ava","Bella","Bernadette","Carol","Caroline","Carolyn","Chloe","Claire","Deirdre","Diana","Diane","Donna","Dorothy",
+"Elizabeth","Ella","Emily","Emma","Faith",
+"Felicity","Fiona","Gabrielle","Grace","Hannah","Heather",
+"Irene","Jan","Jane","Jasmine","JenniferJessica",
+"Joan","Joanne","Julia","Karen","Katherine","Kimberly","Kylie","Lauren","Leah","Lillian","Lily","Lisa","Madeleine","Maria","Mary","Megan","Melanie","Michelle","Molly","Natalie","Nicola",
+"Olivia","Penelope","ippa","Rachel",
+"Rebecca","Rose","Ruth","Sally","Samantha","Sarah","Sonia",
+"Sophie","Stephanie","Sue","Theresa","Tracey","Una","Vanessa","Victoria","Virginia","Wanda",
+"Wendy","Yvonne","Zoe"]
+
+
+Country =["Afghanistan","Albania","Algeria","Andorra","Angola","Argentina","Armenia","Australia","Austria","Azerbaijan","Bangladesh",
+"Barbados","Belgium","Belize","Bolivia","Botswana","Brazil","Cambodia","Cameroon","Canada","Chile","China","Colombia","Comoros","CongoFreeState","CostaRica",
+"Croatia","Cuba","Cyprus","Czechia","Czechoslovakia","EastGermany","Ecuador","Egypt","ElSalvador","EquatorialGuinea",
+"Eritrea","Estonia","Eswatini","Ethiopia","Fiji","Finland","France","Germany","Ghana","Greece","Hungary",
+"Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Libya","Madagascar","Malaysia","Maldives","Mali","Malta","Mexico","Mongolia",
+"Nepal","Netherlands","The","NewZealand","Norway","Pakistan","Palau","Panama","Portugal","Qatar","Romania","Russia","Rwanda","SaintLucia","SaudiArabia",
+"Schaumburg-Lippe","Senegal","Serbia","Slovakia","Slovenia","SolomonIslands","The","Somalia","SouthAfrica","SouthSudan","Spain","SriLanka","Sudan","Suriname","Sweden","Switzerland","Syria",
+"Tajikistan","Tanzania","TrinidadandTobago","Tunisia","Turkey","UnitedArabEmirates","UnitedKingdom","Vanuatu","Venezuela","Vietnam","Württemberg","Yemen","Zambia","Zimbabwe"]
